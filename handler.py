c480120ad11e182fae48fea114460f5356bd6335
nebokecat <neboke.cat@gmail.com>
2022-10-18 17:01:29 +0900

    importがクリアしたことを確認

--- handler.py
+++ handler.py
@@ -1,7 +1,17 @@
+import os
 import json
+from slack_bolt import App
 
+SLACK_BOT_TOKEN=os.getenv('SLACK_BOT_TOKEN')
+SLACK_SIGNING_SECRET=os.getenv('SLACK_SIGNING_SECRET')
+
+app = App(
+    token=SLACK_BOT_TOKEN,
+    signing_secret=SLACK_SIGNING_SECRET
+)
 
 def hello(event, context):
+    print(app)
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
